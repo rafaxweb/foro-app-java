380080325b693c46150bac0a2bb7b7cc65f40ae5
Salas <rafael.salas@solera.com>
2022-10-05 15:08:36 +0200

    Banned Words

---
+++ src/main/java/com/solera/forum2/model/BannedWords.java
@@ -0,0 +1,36 @@
+package com.solera.forum2.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+public class BannedWords {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	long id;
+	
+	@Column
+	String word;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+	
+}
